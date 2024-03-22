e3c94acdbeecdd5a0be6a8e604f31169013b2f99
Sonupr130 <sonupradhan130@gmail.com>
2024-03-22 20:42:21 +0530

    add onChange and onSubmit functionality to the search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
 
 
   return (
-    <header className="text-slate-200 shadow-md">
+    <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
@@ -44,7 +44,9 @@ const Header = () => {
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/">
